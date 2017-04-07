1614bc41fffb66bcdf0d64db1b101eb5637ba53f
sseen <ssnfee@gmail.com>
2017-04-07 17:49:27 +0800

    Add

--- ExpandSectionTableDemo/ViewController.swift
+++ ExpandSectionTableDemo/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,5 +21,36 @@ class ViewController: UIViewController {
     }
 
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "")
+        
+        cell.textLabel?.text = "\(indexPath.row)"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView(frame: CGRect.zero)
+        
+        // align view from the left
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-0-[view]", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["view": view]));
+        
+        // align view from the top
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-0-[view]", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["view": view]));
+        
+        // width constraint
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:[view(==100)]", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["view": view]));
+        
+        // height constraint
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[view(==20)]", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["view": view]));
+    }
 }
 
