206a2373c57afef04a5ba4a0b2286c649d4fce76
sseen <ssnfee@gmail.com>
2017-04-10 17:44:32 +0800

    Reload done

--- ExpandSectionTableDemo/ViewController.swift
+++ ExpandSectionTableDemo/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var dataSource = [[1,2,3],[4,5,6]]
+    @IBOutlet weak var mainTable: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +24,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return dataSource[section].count
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return dataSource.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -35,6 +37,12 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         cell.textLabel?.text = "\(indexPath.row)"
         
         return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
@@ -46,6 +54,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let lbl = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 40))
         let imv = UIImageView(image: #imageLiteral(resourceName: "webForward.pdf"))
     
+        view.tag = section
         view.backgroundColor = UIColor.cyan
         lbl.text = "hel"
         lbl.textColor = UIColor.black
@@ -54,9 +63,27 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         view.addSubview(lbl)
         view.addSubview(imv)
         
-        
+        // add gesture
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ckHeader(_:)))
+        tapGesture.numberOfTapsRequired = 1
+        view.addGestureRecognizer(tapGesture)
         
         return view
+    }
+    
+    func ckHeader(_ sender:UIGestureRecognizer) {
+        let index:Int = (sender.view?.tag)!
+        
+        mainTable.beginUpdates()
+        
+        if dataSource[index].count == 0 {
+            dataSource[index].insert(contentsOf: [1,2,3], at: 0)
+        } else {
+            dataSource[index].removeAll()
+        }
+        
+        mainTable.reloadSections(IndexSet.init(integer: index), with: .automatic)
+        mainTable.endUpdates()
     }
 }
 
